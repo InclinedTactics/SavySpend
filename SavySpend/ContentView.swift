dac547399b73ea380246b46dffde34c17755e383
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-03-19 16:02:27 -0500

    Initial Commit

---
+++ SavySpend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SavySpend
+//
+//  Created by J. DeWeese on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SavySpend/SavySpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavySpendApp.swift
+//  SavySpend
+//
+//  Created by J. DeWeese on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SavySpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
